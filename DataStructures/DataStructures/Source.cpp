b1e79fb96cc181334ba3605025a21c6cd035ba10
Psyke <psykepro@abv.bg>
2018-09-23 18:30:11 +0300

    Moving the implementation of AVLTree to header file.

---
+++ DataStructures/DataStructures/Source.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include "AVLTree.h"
+
+int main()
+{
+
+	//### Test AVL Tree - BEGIN ###\\
+	//AVLTree<int> t;
+	//t.Insert(20);
+	//t.Insert(25);
+	//t.Insert(15);
+	//t.Insert(10);
+	//t.Insert(30);
+	//t.Insert(5);
+	//t.Insert(35);
+	//t.Insert(67);
+	//t.Insert(43);
+	//t.Insert(21);
+	//t.Insert(10);
+	//t.Insert(89);
+	//t.Insert(38);
+	//t.Insert(69);
+	//t.Display();
+
+	//t.Remove(35);
+	//t.Remove(38);
+	//t.Display();
+	//### Test AVL Tree - END ###\\
+
+	std::cin.ignore();
+	std::cin.get();
+	return 0;
+}
